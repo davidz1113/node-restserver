2e93736fb0da75a2f45e8c0cb452ef396f7ba9aa
David Andres <d.carvajal_13@hotmail.com>
2019-02-26 15:26:11 -0500

    primer comit

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+require('./config/config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre == undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({ persona: body })
+    }
+});
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+
+});
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario')
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('ecuchando puerto 3000');
+});
