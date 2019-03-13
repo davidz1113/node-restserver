6bd828c6f7895a59a42e28827a4a7e0f5f2757ef
David Andres <d.carvajal_13@hotmail.com>
2019-03-13 14:05:55 -0500

    configurando el config.js para conexion remota con mongoAtlas

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,27 @@
 //  =============================
 process.env.PORT = process.env.PORT || 3000;
 //mongodb://localhost:27017/cafe
-process.env.urlDB = 'mongodb://localhost:27017/cafe';
+
+
+/**=====================
+ * Entorno
+ * ==================
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/**================
+ * Base de datos
+ * Valida si esta en dessarrollo o en produccion, 
+ * ==============
+ */
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://death-stroke:op6IBi3p2uHxZqIG@cluster0-vclgj.mongodb.net/cafe'
+}
+
+process.env.urlDB = urlDB;
