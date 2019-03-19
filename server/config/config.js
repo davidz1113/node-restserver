80838de32994729c04d80c1f1fb3eafef1bc04cf
David Andres <d.carvajal_13@hotmail.com>
2019-03-19 11:43:09 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://death-stroke:op6IBi3p2uHxZqIG@cluster0-vclgj.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
