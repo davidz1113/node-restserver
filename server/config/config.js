847096dcf611a086c8385b336079f23cdb967065
David Andres <d.carvajal_13@hotmail.com>
2019-06-12 15:54:05 -0500

    agregando google sing in

--- server/config/config.js
+++ server/config/config.js
@@ -47,3 +47,11 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+
+/**=====================
+ * Google Client ID
+ * ==================
+ */
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '378656333429-1kfm4jkisdod122ur817rfshlcnutste.apps.googleusercontent.com';
