00bb3f80452009180ca076a6147edf16a2c121cb
Andrés Carvajal <d.carvajal_13@hotmail.com>
2019-06-16 22:57:08 -0500

    empezando con categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+let { verificaToken } = require('../middlewares/autenticacion')
+
+let app = express();
+
+
+let Categoria = require('');
+
+/**
+ * =========================
+ * Mostrar todas las categorias
+ * =========================
+ */
+
+
+
+ 
+/**
+ * =========================
+ * Mostrar las categorias por ID
+ * =========================
+ */
+
+
+
